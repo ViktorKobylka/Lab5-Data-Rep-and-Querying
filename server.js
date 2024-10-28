04269109c164fee1e6d6c06aa2b127a5eb81fc8a
ViktorKobylka <g00425163@atu.ie>
2024-10-28 17:54:14 +0000

    added get request for /name

--- server.js
+++ server.js
@@ -60,3 +60,9 @@ const path = require('path');
 app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/name', (req, res) => {
+    const firstname = req.query.firstname;
+    const lastname = req.query.lastname;
+    res.send(`Hello ${firstname} ${lastname}`);
+});
